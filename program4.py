d0e90001f36307d2c38b11e7698b6e5d71b5f3f4
Chelsea Chen <chelseachen893@gmail.com>
2020-07-07 00:34:42 -0600

    program 4

---
+++ program4.py
@@ -0,0 +1,11 @@
+"""
+Assignment: Given a list [12,35,9,56,34] , interchange first and last element in the list
+Date created: 07/06/2020
+Author: Chelsea Chen
+"""
+values = [12,35,9,56,34]
+print("original: " + str(values))
+values[0] = 34
+values[4] = 12
+
+print("new: " + str(values))
